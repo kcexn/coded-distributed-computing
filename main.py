49874d348687f53ac1b5d6e9635d18884630baff
Kevin Exton <kexton@student.unimelb.edu.au>
2020-11-06 17:31:27 +1100

    Following Jingge's paper I've implemented up to the part where the A matrix has been divided into j parts.

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
+from sys import stderr,exit
+
 import numpy as np
+from scipy import linalg
 
 from coded_distributed_computing import encode_matrix
 
 
 if __name__ == "__main__":
-    A = np.matrix('1 0; 0 0')
-    G = np.matrix('1 0 ; 0 1; 1 1')
-    AG = encode_matrix(G,A)
-    print(AG)
+    #K = 49, NUM_OF_DIVISION = 8 is a good edge case to consider where the 8th matrix is empty with shape 0x30
+    K = 60
+    N = 30
+    SCALE_FACTOR = 2**10
+    NUM_OF_MATRICES = 1
+    NUM_OF_PROCESSORS = 1
+    MAX_NUM_OF_DIVISIONS = K//NUM_OF_MATRICES + 1
+    NUM_OF_DIVISIONS = 2
+
+    try:
+        assert NUM_OF_DIVISIONS < MAX_NUM_OF_DIVISIONS
+    except AssertionError as error:
+        print(f'The maximum number of matrix divisions is {MAX_NUM_OF_DIVISIONS}. NUM_OF_DIVISIONS is too high.', file=stderr)
+        exit(1)
+
+    H = SCALE_FACTOR*np.random.random((K,N))-(SCALE_FACTOR/2)*np.ones((K,N))
+    x = SCALE_FACTOR*np.random.random((N,1))
     
+    reference_solution = np.matmul(H,x)
+    #  if K%2 == 0 else H[(K//NUM_OF_DIVISIONS+1)*i:(K//NUM_OF_DIVISIONS+1)*(i+1),:]
+
+    A_matrices = [
+        np.asmatrix(
+            H[K//NUM_OF_DIVISIONS*i:K//NUM_OF_DIVISIONS*(i+1),:] if K%NUM_OF_DIVISIONS == 0 else H[(K//NUM_OF_DIVISIONS+1)*i:(K//NUM_OF_DIVISIONS+1)*(i+1),:]
+        ) for i in range(NUM_OF_DIVISIONS)
+    ]
+
+    print(A_matrices[7])
